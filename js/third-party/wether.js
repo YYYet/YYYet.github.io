0cf2788ec6bbae1f90351f2e4b5c66c18f119ebc
YYYet <153680858@qq.com>
2020-08-18 18:35:18 +0800

    Site updated: 2020-08-18 18:35:13

---
+++ js/third-party/wether.js
@@ -0,0 +1,22 @@
+
+WIDGET = {
+  CONFIG: {
+    "modules": "01234",
+    "background": 1,
+    "tmpColor": "FFFFFF",
+    "tmpSize": 16,
+    "cityColor": "FFFFFF",
+    "citySize": 16,
+    "aqiSize": 16,
+    "weatherIconSize": 24,
+    "alertIconSize": 18,
+    "padding": "10px 10px 10px 10px",
+    "shadow": "1",
+    "language": "auto",
+    "borderRadius": 5,
+    "fixed": "false",
+    "vertical": "middle",
+    "horizontal": "center",
+    "key": "rjYdBwtYdo"
+  }
+}
